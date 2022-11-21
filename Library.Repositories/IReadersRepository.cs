ad4b4e58c9d3ce6b91436a1a919ae998de0281ff
xe <111978914+tetttet@users.noreply.github.com>
2022-11-21 21:03:14 +0600

    Update IReadersRepository.cs

--- Library.Repositories/IReadersRepository.cs
+++ Library.Repositories/IReadersRepository.cs
@@ -1,4 +1,6 @@
-﻿using Library.Entities;
+using Library.Entities;
+using System.Data.SqlClient;
+
 namespace Library.Repositories;
 
 public interface IReadersRepository {
@@ -6,9 +8,12 @@ public interface IReadersRepository {
   List<Reader> GetReaderCity(int cityId);
   List<Reader> GetReaderIIN(string iin);
   int ReadersCount(int cityid);
+  Reader GetReaderById(int id);
+  void DeleteReaderById(int id);
 }
 
 public class LinqToReadersRepository : IReadersRepository {
+  string connectionString = @"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Library;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
   public int GetReaderCount() {
     return DataStorage.GetReaders().Count;
   }
@@ -20,6 +25,16 @@ public class LinqToReadersRepository : IReadersRepository {
   }
   public int ReadersCount(int cityid) {
     return DataStorage.GetReaders().Where(reader => reader.CityId == cityid).Count();
+  }
+  public Reader GetReaderById(int id) {
+    return DataStorage.GetReaders().First(reader => reader.Id == id);
+  }
+  public void DeleteReaderById(int id) {
+    using (SqlConnection conn = new SqlConnection(connectionString))
+    using (SqlCommand command = new SqlCommand("DELETE FROM Readers WHERE Id = '" + id + "'", conn)) {
+      conn.Open();
+      command.ExecuteNonQuery();
+    }
   }
 
 }
